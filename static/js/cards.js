d93014094bb2257a967502a799f74c67145fe7f6
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-04-16 18:24:52 +0400

    The Final

---
+++ static/js/cards.js
@@ -0,0 +1,4 @@
+const cardsContainer = document.querySelector(".cards-container");
+const image = document.createElement("img");
+const imageSrc = "";
+image.src = "../";
---
+++ static/js/modal.js
@@ -0,0 +1,11 @@
+$(function()
+{
+    $('#show-modal').click(function(){
+        $('#send-message').fadeIn();
+    });
+
+    $('#close-modal').click(function(){
+        $('#send-message').fadeOut();
+    });
+
+});
