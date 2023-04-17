f771bb726c5223e6afb1ee1d52091b1ceebf9646
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-04-17 21:17:06 +0400

    m

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.querySelector(".burger-menu-closed");
-const burgerMenuOpend = document.querySelector(".burger-menu-opened");
-burgerMenu.addEventListener("click", function(){
-    console.log(5);
-    burgerMenuOpend.classList.toggle("active-burger");
-});
