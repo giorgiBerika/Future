c068484e59f3749f787c5b5214002743df21a5d3
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-04-19 22:06:25 +0400

    us

--- static/js/hamburger-menu.js
+++
@@ -1,6 +0,0 @@
-const menuBar = document.getElementById("menu-bar");
-  menuBar.addEventListener("click", function(){
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  })
